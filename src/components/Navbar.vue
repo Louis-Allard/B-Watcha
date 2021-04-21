7253e357bab928e702e05b34ec8655498095d22b
Louis Allard <la.devweb@yahoo.com>
2021-04-21 17:38:20 +0200

    Call movies list.

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -6,7 +6,8 @@
         class="nav-link"
         v-for="(genre, key) in genres"
         :key="key"
-        >{{ genre }}</b-nav-item
+        @click="discover(genre.id)"
+        >{{ genre.name }}</b-nav-item
       >
     </b-navbar-nav>
   </b-navbar>
@@ -20,6 +21,7 @@ export default {
   data() {
     return {
       genres: [],
+      movies: []
     };
   },
   methods: {
@@ -31,11 +33,24 @@ export default {
         .then((response) => {
           const genres = response.data["genres"];
           for (let i = 0; i < genres.length; i++) {
-            this.genres.push(genres[i]["name"]);
+            this.genres.push(genres[i]);
           }
-          console.log(this.genres);
         });
     },
+    discover(g) {
+      axios
+        .get(
+        `https://api.themoviedb.org/3/discover/movie?api_key=9132ab9c1b6b3b76b975589da594226b&language&language=en-US&sort_by=popularity.desc&include_adult=false&include_video=true&with_genres=` + g
+        )
+        .then((response) => {
+          const movies = response.data["results"]
+          this.movies = [];
+          for (let i = 0; i < movies.length; i++) {
+            this.movies.push(movies[i]["original_title"]);
+          }
+          console.log("movies : ", this.movies);
+        });
+    }
   },
   mounted() {
     this.load();
