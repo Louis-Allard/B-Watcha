2bb4aa07f1a074df6f653e90ae2d328c98ca9122
Louis Allard <la.devweb@yahoo.com>
2021-04-28 14:27:23 +0200

    Add Main component.

---
+++ src/components/Main.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    Hello World
+    <p></p>
+  </div>
+</template>
+
+<script>
+import "../sass/global.scss";
+export default {
+  name: "Main",
+  data() {
+    return {
+    };
+  },
+  methods: {
+      test(){
+        const text = localStorage.getItem;
+        console.log("Response: " + text)
+        }      
+      },
+  mounted() {
+      this.test();
+  },
+};
+</script>
